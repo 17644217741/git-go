dec3874e31fc3309888e82d9cbe3ae742f65d1d8
suijiqing <suijiqing@smm.cn>
2022-06-07 17:28:51 +0800

    修改

--- test.go
+++ test.go
@@ -5,6 +5,7 @@ import(
 	_ "github.com/go-sql-driver/mysql"
 	"io/ioutil"
 	"net/http"
+	"encoding/json"
 )
 //数据库配置
 cinst(
@@ -17,6 +18,7 @@ cinst(
 //数据库连接池
 var DB *sql.DB
 
+//初始化数据库
 func InitDB(){
 	//构建连接
     path :=strings.join([]string{userName,":",password,"@tcp(",ip,":",port,")/",dbName,"?charset=utf8"},"")
@@ -35,8 +37,27 @@ func InitDB(){
 
 }
 
+//返回参数结构体
+type Result strust{
+	Rowtitle string `json:"rowtitle"`
+	Businessdates string `json:"businessdates"`
+	Values string `json:"values"`
+	Hoverext string `json:"hovertext"`
+}
+
+//执行入口
 func main(){
-    InitDB()
+    InitDB()//数据库初始化
+	result := httpGet()//调用接口
+	//遍历结果入表
+	for k, v := range result["Rows"] {
+		fmt.Printf("k=%v, v=%v\n", k, v)
+		//循环结果入表
+	    insertT()
+	}
+}
+
+func httpGet() interface{}{
 	//调用接口
 	resp, err :=http.Get("https://www.lme.com/api/trading-data/day-delayed?datasourceId=2a431297-6620-4ba7-a991-8335423f994b")
 	//检查接口是否通了
@@ -45,11 +66,19 @@ func main(){
 		return
 	}
 	defer resp.Body.Close()
+
 	//输出结果
-	body,_ :=ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
-	var res result
-	_ = json.Unmarshal(body,&res)
-	fmt.Printf("%#v",res)
+	body, err :=ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	result := map[string]interface{}{}
+	json.Unmarshal(body, &result)
+	return 	result 
+}
+
+//json入表
+func insertT(){
 
 }
